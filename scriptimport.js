ee2fd4b11318d17f02a44725aabfcd4d5a77f5a6
hupratt <hprattdo@gmail.com>
2023-02-27 20:02:27 +0100

    adding migration data

--- scriptimport.js
+++ scriptimport.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
-var ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
+const csv = require("fast-csv");
+
+
 var path = require("path");
 const extractFrames = require('ffmpeg-extract-frames')
 const pg = require("pg");
@@ -59,6 +62,42 @@ User.hasMany(Subscription, {
 User.belongsToMany(Video, { through: View, foreignKey: "userId" });
 Video.belongsToMany(User, { through: View, foreignKey: "videoId" });
 
+const options = {
+  objectMode: true,
+  delimiter: ",",
+  quote: null,
+  headers: false,
+  renameHeaders: false,
+};
+const data = [];
+
+async function asyncCallReadCSV() {
+  var stream = fs.createReadStream("migration_data.csv");
+  await csv
+  .parseStream(stream, {headers : false, delimiter: '/n'})
+  .on("data", function(data){
+      console.log('I am one line of data', data.arg1);
+  })
+  .on("end", function(){
+      console.log("done");
+  });
+}
+
+asyncCallReadCSV()
+// var csvData=[];
+// fs.createReadStream(path.join(__dirname,"migration_data.csv"))
+//   .pipe(parse({ delimiter: "," }))
+//   .on("data", function (row) {
+//     console.log(row);
+//     csvData.push(row);  
+//   })
+//   .on("end", function () {
+//     console.log("finished");
+//   })
+//   .on("error", function (error) {
+//     console.log(error.message);
+//   });
+
 const relPath='uploads/netgear/Videos/1999 part 2/VTS_01_1.m4v'
 var fileName = path.basename(relPath);
 
@@ -75,12 +114,9 @@ async function asyncCall() {
     title: 'test',
     description: '',
     url: 'uploads/netgear/Videos/1999 part 2/VTS_01_1.m4v',
-    thumbnail: path.join('static', path.dirname(relPath),`${fileName}-frame-%5.png`),
+    thumbnail: path.join('static', path.dirname(relPath),`${fileName}-frame-5.png`),
   }).then(function(item){
-    res.json({
-      "Message" : "Created item.",
-      "Item" : item
-    });
+    console.log("Created item.")
   }).catch(function (err) {
     console.log(err)
   });
