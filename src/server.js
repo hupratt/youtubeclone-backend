68095fba168404aa2c322b7aca5ef370db04f3ba
hupratt <hprattdo@gmail.com>
2023-08-21 16:59:35 +0200

    fix CORS issue

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ var path = require('path');
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: true }));
+app.use(cors());
 
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/admin", admin);
